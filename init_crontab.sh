c9426add21b1d4b244fd3a19b832fbf570446179
Fabio Del Bene <delbio87@gmail.com>
2014-07-15 23:50:22 +0200

    correzione bug riferimento file

--- init_crontab.sh
+++ init_crontab.sh
@@ -1,4 +1,13 @@
-command="sh $(pwd)/sendIP.sh ${1}"
+
+recipient_file=${1}
+
+if [ ! -f $recipient_file ];
+then
+        echo "Selected Recipient file: $recipient_file not found"
+        exit
+fi
+
+command="cd $(pwd); bash sendIP.sh ${recipient_file}"
 job="@reboot ${command}"
 cat <(fgrep -i -v "$command" <(crontab -l)) <(echo "$job") | crontab -
 crontab -l
