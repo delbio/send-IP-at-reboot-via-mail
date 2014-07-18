7653efa3661a895836087a828916dde5c1f6176e
Delbio <delbio87@gmail.com>
2014-07-18 12:44:25 +0200

    controllo che le interfacce di rete siano pronte prima di inviare la mail con il nuovo ip

--- sendIP.sh
+++ sendIP.sh
@@ -2,6 +2,38 @@
 
 set -e # exit on error
 
+# http://www.linuxjournal.com/content/return-values-bash-functions
+
+function testConnection()
+{
+#    echo "Test connection for network device $1"
+    local it="indirizzo inet:"
+    local en="inet addr:"
+    local pattern_match=$it
+    if ifconfig ${1} 2<&1 | grep -q "$it" ; then
+        echo "true"
+    else
+        echo "false"
+    fi  
+}
+
+function connectionIsReady()
+{
+    COUNTER=true
+    while $COUNTER; do
+        if [ $COUNTER ]; then
+            echo "Rete non disponibile ..."
+        else
+            echo "Rete dispobile ;)"
+        fi  
+
+        if $(testConnection wlan0) || $(testConnection eth0); then
+           COUNTER=false
+        fi  
+    done
+}
+echo "Rete dispobile ;)"
+
 function sendMailFromRecipientFile() {
         recipient_file=${1}
 
@@ -11,13 +43,15 @@ function sendMailFromRecipientFile() {
                 exit
         fi
 
+        connectionIsReady
+        echo "Rete dispobile ;)"
+        
         while read email
         do
                 target=$email
 
                 echo "Send mail to: $target"
-
-                subject="New HostName"
+                subject="New IP"
                 sh printIP.sh 2<&1 | mail -s "${subject}" ${target}
         done < $recipient_file
 }
